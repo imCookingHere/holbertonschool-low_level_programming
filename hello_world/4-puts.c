80b6042bdb654fdadc1a868e7850f446d7577e1a
Jack MacDonald <9902@holberstonstudents.com>
2024-10-21 03:45:10 +0000

    Task 4 made Betty very happy

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,19 @@
-#include <stdio.h>
-
-/*Function: prints_text
- *--------------------
- *Prints the exact text required to complete task 4
+/*
+ * File: 4-puts.c
+ * Auth: Jack MacDonald <9902@holberstonstudents.com>
  */
 
+#include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
