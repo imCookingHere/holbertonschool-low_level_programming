c3958edc66483b1e4e0ee2f11eed66c679fc5799
Jack MacDonald <9902@holberstonstudents.com>
2024-10-21 03:41:23 +0000

    Task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/*Function: prints_text
+ *--------------------
+ *Prints the exact text required to complete task 4
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
