507f3cf74e03d724af19fadf10c0cb7bb43ca1ee
Jack MacDonald <9902@holberstonstudents.com>
2024-10-22 01:30:36 +0000

    Task 1 added else, else if

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -24,11 +24,11 @@ int main(void)
 	{
 	printf("Last digit of %d is %d and is greater than 5\n", n, ld);
 	}
-	if (ld == 0)
+	else if (ld == 0)
 	{
 	printf("Last digit of %d is %d and is 0\n", n, ld);
 	}
-	if (ld < 6)
+	else
 	{
 	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
 	}
