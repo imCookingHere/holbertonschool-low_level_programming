3102032afa5c9b7d7018b8d8ec535e2edda43ee4
Jack MacDonald <9902@holberstonstudents.com>
2024-10-25 05:05:31 +0000

    Task 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - checks for a lowercase character
+ * @c: is character to check
+ * Return: 1 if c is lowercase
+ */
+
+	int _islower(int c)
+{
+	return (c >= 'a' && c < 'z');
+}
