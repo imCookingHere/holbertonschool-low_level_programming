b7093ed4b696615e7bfe7a4e1741b005a4701b55
Jack MacDonald <9902@holberstonstudents.com>
2024-10-25 04:39:33 +0000

    Task 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
+ */
+
+	void print_alphabet_x10(void)
+{
+	int i = 0, c;
+
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+
+		_putchar('\n');
+		i++;
+	}
+}
