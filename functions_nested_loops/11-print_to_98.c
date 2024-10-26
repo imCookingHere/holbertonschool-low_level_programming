e215955b7e5239381ebeb06e8a993f4067fb9552
Jack MacDonald <9902@holberstonstudents.com>
2024-10-26 02:34:39 +0000

    Task 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -14,18 +14,24 @@
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ", i);
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
 		}
 	}
 	else if (n > 98)
 	{
 		for (i = n; i >= 98; i--)
 		{
-			printf("%d, ", i);
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
 		}
 	}
 	else
 	{
-		printf("%d, ", n);
+		printf("%d\n",  n);
 	}
 }
