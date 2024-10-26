a442b0606c1b9959db82d3429da82dba51ce18ab
Jack MacDonald <9902@holberstonstudents.com>
2024-10-26 01:41:14 +0000

    Task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two intergers and returns the result
+ * @a: first int to check
+ * @b: second int to check
+ * Return: the value of two added intergers
+ */
+
+int add(int a, int b)
+{
+	int c = (a + b);
+
+	return (c);
+}
