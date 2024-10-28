bf04b446c884455d0f594be4f4690c8a4a73b82c
Jack MacDonald <9902@holberstonstudents.com>
2024-10-28 01:06:45 +0000

    Task 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two integers
+ * Return: Always 0
+ * @a: int to be checked
+ * @b: int to be checked
+ */
+
+	int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
