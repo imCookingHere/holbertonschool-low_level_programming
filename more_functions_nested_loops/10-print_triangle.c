045f693d67607fc3b385381985b9b9b70aed9f75
Jack MacDonald <9902@holberstonstudents.com>
2024-10-29 12:32:17 +0000

    Task 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_triangle: prints a triangle followed by a new line
+ * size: size of the triangle
+ */
+
+	void print_triangle(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	
+	for (a = 1; a <= size; a++)
+	{
+		for (b = 1; b <= size; b++)
+		{
+			if (b <= size - a)
+			_putchar(' ');
+			else
+			_putchar('#');	
+		}
+		_putchar('\n');
+	}
+}
