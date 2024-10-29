119c521ad31acb1fcbb2276942c263d0ac79f0c8
Jack MacDonald <9902@holberstonstudents.com>
2024-10-29 11:38:17 +0000

    Task 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square followed by a new line
+ * @size: is the size of the square
+ */
+
+	void print_square(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+
+	for (a = 0; a < size; a++)
+	{
+		for (b = 0; b < size; b++)
+		{
+			_putchar(35);
+		}
+	_putchar('\n');
+	}
+}
