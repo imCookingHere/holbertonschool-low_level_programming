4125852076de48986899b195acf2f7764af04c53
Jack MacDonald <9902@holberstonstudents.com>
2024-11-01 23:56:27 +0000

    Task 5 fixed

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,25 +1,31 @@
 #include "main.h"
 
 /**
- * print_rev - check the code
+ * rev_string - check the code
  * @s: pointer to the string to reverse
  *
  * Return: reverses the string passed in.
  */
-
-	void rev_string(char *s)
+void rev_string(char *s)
 {
 	int count = 0;
+	int length;
+	char temp;
 
 	while (s[count] != '\0')
 	{
 		count = count + 1;
 	}
 
-	while (count > 0)
+	count = count - 1;
+	length = count;
+
+	while (count > (length / 2))
 	{
+		temp = s[count];
+		s[count] = s[length - count];
+		s[length - count] = temp;
+
 		count = count - 1;
-		_putchar(s[count]);
 	}
-	_putchar('\n');
 }
