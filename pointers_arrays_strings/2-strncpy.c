70102512af7579a458fc75990c6a96bce6e33303
Jack MacDonald <9902@holberstonstudents.com>
2024-11-02 22:30:25 +0000

    Task 2, strncpy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ *
+ * @dest: string output location
+ * @src: string to be copied
+ * @n: length of the string
+ *
+ * Return: dest
+ */
+
+	char *_strncpy(char *dest, char *src, int n)
+{
+	int a = 0;
+
+	while (a < n && src[a] != '\0')
+	{
+		dest[a] = src[a];
+		a++;
+		src++;
+	}
+	while (a < n)
+	{
+		dest[a] = '\0';
+		a++;
+	}
+	return (dest);
+}
