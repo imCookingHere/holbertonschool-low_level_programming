75f9b084e08c3774ce86ea6e337c599301952638
Jack MacDonald <9902@holberstonstudents.com>
2024-11-02 00:53:06 +0000

    Task 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * print_array - prints n elements of an array of integers followed by a nl
+ * @a: pointer at array
+ * @n: arrays to be counted
+ */
+
+	void print_array(int *a, int n)
+{
+	int count = 0;
+
+	while (count < n)
+	{
+	printf("%d, ", *a);
+	a++;
+	count++;
+	}
+	_putchar('\n');
+}
