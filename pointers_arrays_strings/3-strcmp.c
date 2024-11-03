c22fb0ce583eb7e04decb0375180dab50ff4969f
Jack MacDonald <9902@holberstonstudents.com>
2024-11-03 00:07:35 +0000

    Task 3, strcmp

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings
+ * @s1: first string to compare
+ * @s2: second string to compare
+ * Return: r
+ */
+
+	int _strcmp(char *s1, char *s2)
+{
+	int ta1 = 0, ta2 = 0;
+
+	while (*s1 != '\0')
+	{
+		s1++;
+		ta1++;
+	}
+	while (*s2 != '\0')
+	{
+		s2++;
+		ta2++;
+	}
+	s1 = 0;
+	s2 = 0;
+
+	if (ta1 > ta2)
+	{
+		return (-15);
+	}
+	else if (ta2 > ta1)
+	{
+		return (15);
+	}
+	else
+	{
+		return (0);
+	}
+}
