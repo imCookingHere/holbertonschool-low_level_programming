70f4b9242bf14d8e73fb80b07e2add861bc36828
Jack MacDonald <9902@holberstonstudents.com>
2024-11-05 04:02:32 +0000

    Task 0, memset

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: pointer to memeory to be filled
+ * @b: data to be inputted
+ * @n: number of bytes to be filled in memory
+ *
+ * Return: s
+ */
+
+	char *_memset(char *s, char b, unsigned int n)
+{
+	char *temp = s;
+	int i = 0;
+	int m = n;
+
+	while (i <= m)
+	{
+		*s = b;
+		i++;
+		s++;
+	}
+	s = temp;
+	return (s);
+}
