38ce860eca01fd01cfba486191c5b04ddccd0edc
Jack MacDonald <9902@holberstonstudents.com>
2024-11-09 01:45:39 +0000

    Task 0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,11 +2,15 @@
 #include "main.h"
 
 /**
- *
- *
+ * main - prints the name of the funcion
+ * @argc: number of arguments
+ * @argv: array containing arguments as strings
+ * Return: 0
  */
 
 	int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
 }
