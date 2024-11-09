45f1c614196133de2cbaceaae9b266ce57814bcd
Jack MacDonald <9902@holberstonstudents.com>
2024-11-09 01:57:10 +0000

    Task 2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+#include "main.h"
+
+/**
+ * main - prints all arguments received
+ * @argc: counts the number of arguments
+ * @argv: array containing the arguments as strings
+ * Return: 0
+ */
+
+	int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+	}
