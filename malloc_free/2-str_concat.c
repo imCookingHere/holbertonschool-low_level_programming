963597f1dd901f87deadc9909d2c041ee128cc95
Jack MacDonald <9902@holberstonstudents.com>
2024-11-12 09:45:37 +0000

    Task 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -11,19 +11,16 @@
 
 	char *str_concat(char *s1, char *s2)
 {
-	int count1 = 0;
-	int count2 = 0;
-	int len1 = 0;
-	int len2 = 0;
+	int count1 = 0, count2 = 0, len1 = 0, len2 = 0;
 	char *p;
 
 	if (s1 == NULL)
 	{
-		return (NULL);
+		s1 = " ";
 	}
 	if (s2 == NULL)
 	{
-		return (NULL);
+		s2 = " ";
 	}
 	while (s1[len1] != '\0')
 	{
