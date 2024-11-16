1e16338d7283ad362cc6a55217b55e7a03600fae
Jack MacDonald <9902@holberstonstudents.com>
2024-11-16 23:26:34 +0000

    Task 5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - Frees memory allocated for a dog_t structure.
+ * @d: The dog_t structure to free.
+ *
+ * Description: Frees the memory allocated for the name and owner strings,
+ *              and then frees the structure itself.
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+	free(d->name);
+	free(d->owner);
+	free(d);
+	}
+}
